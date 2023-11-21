8e59a3d58070b7b5cce34a308efa175ec43e4007
34n0 <34n0@immerda.ch>
2023-11-21 02:47:05 +0100

    refactor: repo to repo package

--- cmd/disable.go
+++ cmd/disable.go
@@ -1,7 +1,7 @@
 package cmd
 
 import (
-	"github.com/34N0/rpm-copr/pkg/repo"
+	"github.com/34N0/rpm-copr/pkg/repos"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +10,7 @@ var disableCmd = &cobra.Command{
 	Use:   "disable",
 	Short: "Disable the name/project Copr repository",
 	Run: func(cmd *cobra.Command, args []string) {
-		repo.NewCopr(args).Disable()
+		repos.NewCopr(args).Disable()
 	},
 }
 
--- cmd/enable.go
+++ cmd/enable.go
@@ -4,7 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"github.com/34N0/rpm-copr/pkg/repo"
+	"github.com/34N0/rpm-copr/pkg/repos"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +14,7 @@ var enableCmd = &cobra.Command{
 	Short: "Enable the name/project Copr repository",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		repo.NewCopr(args).Enable()
+		repos.NewCopr(args).Enable()
 	},
 }
 
